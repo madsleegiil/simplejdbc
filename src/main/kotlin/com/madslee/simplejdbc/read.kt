40623bffb84a4f41131211a5051950c6d02de822
Mads Lee Giil <mads.lee.giil@nav.no>
2021-12-20 07:25:34 +0100

    Implement reading from database by specifying class type only

--- src/main/kotlin/com/madslee/simplejdbc/read.kt
+++ src/main/kotlin/com/madslee/simplejdbc/read.kt
@@ -2,12 +2,10 @@ package com.madslee.simplejdbc
 
 import com.madslee.simplejdbc.util.*
 import java.sql.Connection
-import java.sql.JDBCType
 import kotlin.reflect.KClass
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
-// TODO: kall på konstruktør er riktig, men typen for string er feil, blir jdbcClob eller noe
 fun <T: Any> getAll(clazz: KClass<T>, connection: Connection) = getAll(clazz, clazz.javaObjectType.simpleName, connection)
 
 fun <T: Any> getAll(clazz: KClass<T>, table: String, connection: Connection): List<Any> =
@@ -16,9 +14,10 @@ fun <T: Any> getAll(clazz: KClass<T>, table: String, connection: Connection): Li
         columns = clazz.memberProperties.map { allLowerCaseSnakeCase(it.name) },
         connection = connection
     ).map { databaseRow ->
-        //clazz.primaryConstructor!!.call("id", "de", 12.3, 1)
         clazz.primaryConstructor!!.call(
-            *databaseRow.valuesWithKeySorting(primaryConstructorParameterNames(clazz).map { allLowerCaseSnakeCase(it) }).values.toTypedArray()
+            *databaseRow.valuesWithKeySorting(
+                primaryConstructorParameterNames(clazz).map { allLowerCaseSnakeCase(it) }
+            ).values.toTypedArray()
         )
     }
 
@@ -28,9 +27,7 @@ fun getAll(table: String, columns: List<String>, connection: Connection): List<M
         .map { resultSet ->
             columns.associateWith {
                 when (resultSet.getObject(it).javaClass.typeName) {
-                    is "Double" -> resultSet.getDouble(it)
-                    is Int -> resultSet.getInt(it)
-                    is String -> resultSet.getString(it)
+                    "org.h2.jdbc.JdbcClob" -> resultSet.getString(it) // TODO: Test with postgres
                     else -> resultSet.getObject(it)
                 }
             }
