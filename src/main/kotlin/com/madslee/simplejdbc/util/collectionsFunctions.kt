6ea897da13885e7222351ae1dfaa169bbea179a3
Mads Lee Giil <mads.lee.giil@nav.no>
2021-12-29 14:08:43 +0100

    Remove unused method

--- src/main/kotlin/com/madslee/simplejdbc/util/collectionsFunctions.kt
+++ src/main/kotlin/com/madslee/simplejdbc/util/collectionsFunctions.kt
@@ -1,15 +1,6 @@
 package com.madslee.simplejdbc.util
 
 
-internal fun <K, V> Map<K, V>.joinWith(overridingEntries: Map<K, V>): Map<K, V> {
-    val newMap: MutableMap<K, V> = overridingEntries.toMutableMap()
-
-    this.forEach { (key, value) ->
-        if (newMap[key] == null) newMap[key] = value
-    }
-    return newMap
-}
-
 internal fun <K, V> Map<K, V>.valuesWithKeySorting(keySorting: List<K>): Map<K, V> {
     val newMap: MutableMap<K, V> = mutableMapOf()
 
