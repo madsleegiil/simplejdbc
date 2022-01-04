78a0c490d91de6fdc5ad9ef441e092a7c7ce84bf
madsleegiil <mads.lee.giil@nav.no>
2022-01-04 20:53:55 +0100

    Renaming to a more general wording

--- src/main/kotlin/com/madslee/simplejdbc/clauses.kt
+++ src/main/kotlin/com/madslee/simplejdbc/clauses.kt
@@ -5,25 +5,25 @@ private val unequalOperator = "!="
 private val isNullOperator = "is null"
 private val isNotNullOperator = "is not null"
 
-fun whereEqual(column: String, match: Any?): Clause {
-    val sql = if (match is Number) numberCompare(column, match, equalOperator)
-        else if (match == null) nullCompare(column, isNullOperator)
-        else generalCompare(column, match, equalOperator)
+fun whereEqual(column: String, compareTo: Any?): Clause {
+    val sql = if (compareTo is Number) numberCompare(column, compareTo, equalOperator)
+        else if (compareTo == null) nullCompare(column, isNullOperator)
+        else generalCompare(column, compareTo, equalOperator)
 
     return Clause(sql)
 }
 
-fun whereNotEqual(column: String, match: Any?): Clause {
-    val sql = if (match is Number) numberCompare(column, match, unequalOperator)
-        else if (match == null) nullCompare(column, isNotNullOperator)
-        else generalCompare(column, match, unequalOperator)
+fun whereNotEqual(column: String, compareTo: Any?): Clause {
+    val sql = if (compareTo is Number) numberCompare(column, compareTo, unequalOperator)
+        else if (compareTo == null) nullCompare(column, isNotNullOperator)
+        else generalCompare(column, compareTo, unequalOperator)
 
     return Clause(sql)
 }
 
-private fun generalCompare(column: String, match: Any?, operator: String) = "$column $operator '$match'"
-private fun numberCompare(column: String, match: Any?, operator: String) = "$column $operator $match"
-private fun nullCompare(column: String, operator: String) = "$column $operator"
+private fun generalCompare(column: String, compareTo: Any?, operator: String) = "$column $operator '$compareTo'"
+private fun numberCompare(column: String, compareTo: Any?, operator: String) = "$column $operator $compareTo"
+private fun nullCompare(column: String, compareTo: String) = "$column $compareTo"
 
 data class Clause(
     val sql: String
