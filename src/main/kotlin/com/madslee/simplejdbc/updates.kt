f2bab7d5e6fe821889565e921397e0e11b5030e4
madsleegiil <mads@giil.no>
2022-01-07 20:50:02 +0100

    Write test for updating item

---
+++ src/main/kotlin/com/madslee/simplejdbc/updates.kt
@@ -0,0 +1,13 @@
+package com.madslee.simplejdbc
+
+import java.sql.Connection
+
+fun Connection.update(any: Any, vararg clause: Clause) {
+
+}
+
+fun Connection.update(field: String, newValue: Any, vararg clause: Clause) {
+
+}
+
+
---
+++ src/test/kotlin/com/madslee/simplejdbc/integration/UpdateTest.kt
@@ -0,0 +1,39 @@
+package com.madslee.simplejdbc.integration
+
+import com.madslee.simplejdbc.insert
+import com.madslee.simplejdbc.select
+import com.madslee.simplejdbc.update
+import com.madslee.simplejdbc.whereEqual
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import javax.sql.DataSource
+
+class UpdateTest: TestSupport() {
+
+    private val dataSource: DataSource = HikariDataSource(
+        HikariConfig().apply {
+            jdbcUrl = "jdbc:h2:mem:test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1"
+            username = "sa"
+            password = ""
+            validate()
+        })
+
+    private val connection = dataSource.connection
+
+    @Test
+    fun `Update price by ID`() {
+        val item = anItem
+        val rowsAffected = connection.insert(item)
+        assertThat(rowsAffected).isEqualTo(1)
+
+        val itemWithUpdatedPrice = anItem.copy(price = 12434343.3)
+        connection.update(itemWithUpdatedPrice, whereEqual("id", anItem.id))
+
+        val allRows = connection.select(Item::class)
+        assertThat(allRows).isEqualTo(1)
+        assertItemsAreEqual(allRows.first(), itemWithUpdatedPrice)
+    }
+
+}
